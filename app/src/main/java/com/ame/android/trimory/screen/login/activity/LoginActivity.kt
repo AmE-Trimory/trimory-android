a09d78f4187a8c777d95def2c8608a4e4c409692
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-15 01:25:41 +0900

    [Add Codes]create login pakage in screen

--- app/src/main/java/com/ame/android/trimory/screen/login/activity/LoginActivity.kt
+++ app/src/main/java/com/ame/android/trimory/screen/login/activity/LoginActivity.kt
@@ -7,6 +7,7 @@ import com.ame.android.trimory.R
 import com.ame.android.trimory.base.BaseActivity
 import com.ame.android.trimory.databinding.ActivityLoginBinding
 import com.ame.android.trimory.screen.login.viewmodel.LoginViewModel
+import com.kakao.sdk.common.KakaoSdk
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
     override val mViewModel: LoginViewModel by viewModels()
@@ -14,6 +15,13 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
     override fun getLayoutRes(): Int = R.layout.activity_login
 
     override fun subscribeUi() {
+        KakaoSdk.init(this, R.string.kakao_native_key.toString())
 
+        with(mViewModel) {
+
+        }
+        with(mDataBinding) {
+            vm = mViewModel
+        }
     }
 }
