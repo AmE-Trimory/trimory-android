c537efcca4b592dd07bfa1be06972d62197ca31a
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-15 00:54:49 +0900

    [Add Codes]set initial api

---
+++ app/src/main/java/com/ame/android/trimory/screen/login/activity/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.ame.android.trimory.screen.login.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.ame.android.trimory.R
+import com.ame.android.trimory.base.BaseActivity
+import com.ame.android.trimory.databinding.ActivityLoginBinding
+import com.ame.android.trimory.screen.login.viewmodel.LoginViewModel
+
+class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
+    override val mViewModel: LoginViewModel by viewModels()
+
+    override fun getLayoutRes(): Int = R.layout.activity_login
+
+    override fun subscribeUi() {
+
+    }
+}
---
+++ app/src/main/java/com/ame/android/trimory/screen/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package com.ame.android.trimory.screen.login.viewmodel
+
+import com.ame.android.trimory.base.BaseViewModel
+
+class LoginViewModel: BaseViewModel() {
+}
