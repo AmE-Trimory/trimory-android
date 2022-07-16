d18751193957341491297a7130c3e6a3ecde517c
Yeeun Lee <leeyeah8245@gmail.com>
2022-07-16 12:29:08 +0900

    [Add Codes]set google login, logout, delete

--- app/src/main/java/com/ame/android/trimory/screen/login/activity/KakaoLoginActivity.kt
+++ app/src/main/java/com/ame/android/trimory/screen/login/activity/KakaoLoginActivity.kt
@@ -1,11 +1,22 @@
 package com.ame.android.trimory.screen.login.activity
 
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.ame.android.trimory.R
 import com.ame.android.trimory.base.BaseActivity
 import com.ame.android.trimory.databinding.ActivityLoginBinding
 import com.ame.android.trimory.screen.login.viewmodel.LoginViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
@@ -13,10 +24,17 @@ import com.kakao.sdk.user.UserApiClient
 
 class KakaoLoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
     override val mViewModel: LoginViewModel by viewModels()
+    private lateinit var mAuth: FirebaseAuth
+    private val mGso: GoogleSignInOptions = GoogleSignInOptions
+        .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken("926945233428-gvscbqf7panoe31s2o5k2i31b5ite3b2.apps.googleusercontent.com")     // R.String.형식으로 가져오면 왜 안되는지 모르겠음 toString()으로 변환해도 안됨
+        .requestEmail()
+        .build()
 
     override fun getLayoutRes(): Int = R.layout.activity_login
 
     override fun subscribeUi() {
+        mAuth = Firebase.auth
         with(mViewModel) {
 
         }
@@ -31,6 +49,23 @@ class KakaoLoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
             mDataBinding.btnKakaoDelete.setOnClickListener {
                 deleteKakao()
             }
+            mDataBinding.btnGoogleLogin.setOnClickListener {
+                loginGoogle()
+            }
+            mDataBinding.btnGoogleLogout.setOnClickListener {
+                logoutGoogle()
+            }
+            mDataBinding.btnGoogleDelete.setOnClickListener {
+                deleteGoogle()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth.currentUser?.let { // 구글 자동 로그인 성공
+            Toast.makeText(this, "구글 자동 로그인 성공", Toast.LENGTH_SHORT).show()
+            Log.i("구글", "${it.email} 자동 로그인 성공")
         }
     }
 
@@ -85,6 +120,47 @@ class KakaoLoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
             else {
                 Log.i("카카오", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
             }
+        }
+    }
+
+    // google Intent 결과
+    private val getgoogleResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            val account = task.getResult(ApiException::class.java)!!
+            registerGoogleUser(account.idToken!!)
+        } catch (e:ApiException){
+            Log.e("구글", "${e} 로그인 실패. 로그인 데이터 없음")
+        }
+    }
+
+    private fun loginGoogle(){
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, mGso)
+        val googleSignIntent: Intent = mGoogleSignInClient.signInIntent
+        getgoogleResult.launch(googleSignIntent)
+    }
+
+    private fun registerGoogleUser(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {task ->
+                Log.i("구글", "구글 로그인 완료")
+            }
+    }
+
+    private fun logoutGoogle(){
+        val googleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(this, mGso)
+        googleSignInClient.signOut().addOnSuccessListener {
+            mAuth.signOut()
+            Log.i("구글", "구글 로그아웃 완료")
+        }
+    }
+
+    private fun deleteGoogle(){
+        mAuth.currentUser?.let {
+            it.delete()
+            Log.i("구글", "사용자 데이터 삭제")
         }
     }
 }
