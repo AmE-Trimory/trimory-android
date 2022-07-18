d7708147c33cdd17922c088ba3b9970e2db732b1
mjh <moomjh94@naber.com>
2022-07-19 00:44:58 +0900

    Compose Ui Test

--- app/src/main/java/com/ame/android/trimory/screen/main/activity/MainActivity.kt
+++ app/src/main/java/com/ame/android/trimory/screen/main/activity/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.ame.android.trimory.screen.main.activity
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import com.ame.android.trimory.R
 import com.ame.android.trimory.base.BaseActivity
 import com.ame.android.trimory.databinding.ActivityMainBinding
 import com.ame.android.trimory.screen.main.viewmodel.MainViewModel
+import java.lang.reflect.Modifier
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
     override val mViewModel: MainViewModel by viewModels()
 
     override fun getLayoutRes(): Int = R.layout.activity_main
@@ -20,4 +33,30 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
             vm = mViewModel
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MainScreen()
+        }
+    }
 }
+
+    @Composable
+    fun MainScreen(){
+        MaterialTheme(
+        ) {
+            Surface(modifier = androidx.compose.ui.Modifier.background(Color.White)) {
+                Column() {
+                    Text("Test")
+                }
+            }
+        }    
+    }
+
+    @Preview
+    @Composable
+    fun PreviewScreen(){
+        MainScreen()
+    }
